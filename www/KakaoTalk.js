5393527b4efa2bad7f9895437efa6599ec91b816
jongwon <jongwons.choi@gmail.com>
2018-01-30 14:07:45 +0900

    Update KakaoTalk.js

--- www/KakaoTalk.js
+++ www/KakaoTalk.js
@@ -1,34 +1,38 @@
-var exec = require('cordova/exec');
 
-var KakaoTalk = {
-	login: function (successCallback, errorCallback) {
-		exec(successCallback, errorCallback, "KakaoTalk", "login", []);
-    },
-	logout: function (successCallback, errorCallback) {
-		exec(successCallback, errorCallback, 'KakaoTalk', 'logout', []);
-	},
+var KakaoTalk = function(){};
 
-	getAccessToken: function (successCallback, errorCallback) {
-		exec(successCallback, errorCallback, 'KakaoTalk', 'getAccessToken', []);
-	},
-
-	share : function(options, successCallback, errorCallback) {
-
-		for(var options_key in options){
-			if(typeof options[options_key] === 'object'){
-				for(var key in options[options_key]){
-					options[options_key][key] = options[options_key][key] || '';
-				};
-			}else{
-				options[options_key] = options[options_key] || '';
-			}
-		};
-	    exec(successCallback, errorCallback, "KakaoTalk", "share", [options]);
-	},
-
-	openPlusFriend: function(id, successCallback, errorCallback) {
-		exec(successCallback, errorCallback, "KakaoTalk", "openPlusFriend", [id]);
-	}
+KakaoTalk.prototype.login = function (successCallback, errorCallback) {
+		cordova.exec(successCallback, errorCallback, "KakaoTalk", "login", []);
+};
+	
+KakaoTalk.prototype.logout = function (successCallback, errorCallback) {
+	cordova.exec(successCallback, errorCallback, 'KakaoTalk', 'logout', []);
 };
 
-module.exports = KakaoTalk;
+KakaoTalk.prototype.getAccessToken = function (successCallback, errorCallback) {
+	cordova.exec(successCallback, errorCallback, 'KakaoTalk', 'getAccessToken', []);
+};
+
+KakaoTalk.prototype.share = function(options, successCallback, errorCallback) {
+
+	for(var options_key in options){
+		if(typeof options[options_key] === 'object'){
+			for(var key in options[options_key]){
+				options[options_key][key] = options[options_key][key] || '';
+			};
+		}else{
+			options[options_key] = options[options_key] || '';
+		}
+	};
+	cordova.exec(successCallback, errorCallback, "KakaoTalk", "share", [options]);
+};
+
+
+KakaoTalk.install = function () {
+  
+	window.KakaoTalk = new KakaoTalk();
+	return window.KakaoTalk;
+};
+
+cordova.addConstructor(KakaoTalk.install);
+  
